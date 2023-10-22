6c75d92f4624f348842ac55b275082084131266f
florentinadili <116742377+florentinaqx@users.noreply.github.com>
2023-10-22 20:30:09 +0200

    Footer handeled

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-kaltert flex items-center justify-between border-t-2 border-portokall">
+            <Link href="/" className="font-bold text-xl">Pizza Mania</Link>
+            <p>@ ALL RIGHTS RESERVED.</p>
+        </div>
+
     )
 }
 
