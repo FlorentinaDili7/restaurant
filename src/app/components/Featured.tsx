19e7d2f6b874f802ff4626d781ed5ee503ba92ab
florentinadili <116742377+florentinaqx@users.noreply.github.com>
2023-12-11 16:33:46 +0100

    Nextjs API CRUD with Prisma

--- src/app/components/Featured.tsx
+++ src/app/components/Featured.tsx
@@ -1,8 +1,24 @@
-import { featuredProducts } from "@/data";
+import { ProductType } from "@/types/types";
 import Image from "next/image";
 import React from "react";
 
-const Featured = () => {
+const getData = async () => {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    const res = await fetch(`${apiUrl}/api/products`, {
+        cache: "no-store"
+    })
+
+    if (!res.ok) {
+        throw new Error("Failed!")
+    }
+    return res.json();
+}
+
+
+const Featured = async () => {
+
+    const featuredProducts: ProductType[] = await getData();
+
     return (
         <div className="w-screen overflow-x-scroll text-red-500">
             {/* WRAPPER */}
