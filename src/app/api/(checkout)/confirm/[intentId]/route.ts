bf491a0064ac0e07989e6cc37b5a41e21d52878e
florentinadili <116742377+florentinaqx@users.noreply.github.com>
2024-01-03 18:25:57 +0100

    fixed build error

--- src/app/api/(checkout)/confirm/[intentId]/route.ts
+++ src/app/api/(checkout)/confirm/[intentId]/route.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/utils/connect';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 export const PUT = async ({ params }: { params: { intentId: string } }) => {
 	const { intentId } = params;
