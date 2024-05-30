ba4d22fa1446029d074995bd9b9aacaa5d5df176
florentinadili <fdili@thequantix.com>
2024-05-30 10:34:18 +0200

    Update

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,6 +5,7 @@ const Footer = () => {
     return (
         <div className="h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-[#F58220] flex items-center justify-between border-t border-portokall">
             <Link href="/" className="font-bold text-xl">PIZZA MANIA</Link>
+            
             <p>© ALL RIGHTS RESERVED.</p>
         </div>
     );
